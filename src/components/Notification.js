11d1ce31b82056161ab1247d5ed6c85d74cc7496
Bartlomiej Gladys <bartekgladys@gmail.com>
2021-01-23 01:04:25 +0100

    Add notification component

---
+++ src/components/Notification.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+export function Notification({ open, setOpen, text, severity = "success" }) {
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+        open={open}
+        autoHideDuration={4000}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity={severity}>
+          {text}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
